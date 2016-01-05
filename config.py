38b3c2ab716eb6ca9b76df004ae31b05964e4780
electronicfreak <github@electronicfreak.de>
2016-01-05 19:29:48 +0100

    quit menu untested

--- config.py
+++ config.py
@@ -11,4 +11,8 @@ btnMap['main'] = ({"pos":(0,0),"dia":(70,100),"img":"menu_header.png"},
 	{"pos":(200,200),"dia":(100,100),"col":(255,128,64),"exec":"music_toggle.sh"},
 	{"pos":(325,200),"dia":(100,100),"col":(255,0,64),"exec":"music_next.sh"},
 	{"pos":(450,200),"dia":(100,100),"col":(0,128,64),"exec":"music_volup.sh"},
-	{"pos":(575,200),"dia":(100,100),"col":(255,128,0),"exec":"music_voldwn.sh"})
+	{"pos":(575,200),"dia":(100,100),"col":(255,128,0),"exec":"music_voldwn.sh"},
+	{"pos":(0,550),"dia":(50,50),"col":255,0,0),"screen":"exit"})
+btnMap['exit'] = ({"pos":(100,100),"dia":(800,400),"col":(255,128,128,128)},
+	{"pos":(150,150),"dia":(325,300),"col":(0,255,0),"exec":"quit.sh"},
+	{"pos":(525,150),"dia":(325,300},"col":(255,0,0),"screen":"main"})
