cab69c1dcb50f78d729b64534971307611784422
electronicfreak <github@electronicfreak.de>
2016-01-06 15:01:12 +0100

    cleanup & config

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ screensaver = 5 * 30
 # ({pos:(x,y),dia:(w,h),col:(R,G,B),text:"text",img:"path",exec:""},...)
 btnMap = {}
 btnMap['main'] = ({"pos":(0,0),"dia":(70,100),"img":"menu_header.png"},
-	{"pos":(200,200),"dia":(100,100),"col":(255,128,64),"exec":"music_toggle.sh"},
-	{"pos":(325,200),"dia":(100,100),"col":(255,0,64),"exec":"music_next.sh"},
-	{"pos":(450,200),"dia":(100,100),"col":(0,128,64),"exec":"music_volup.sh"},
-	{"pos":(575,200),"dia":(100,100),"col":(255,128,0),"exec":"music_voldwn.sh"},
-	{"pos":(0,550),"dia":(50,50),"col":255,0,0),"screen":"exit"})
+	{"pos":(125,100),"dia":(200,75),"img":"btn_music_toggle.png","exec":"music_toggle.sh"},
+	{"pos":(125,180),"dia":(200,75),"img":"btn_music_next.png","exec":"music_next.sh"},
+	{"pos":(125,260),"dia":(200,75),"img":"btn_music_volup.png","exec":"music_volup.sh"},
+	{"pos":(125,340),"dia":(200,75),"img":"btn_music_voldown.png","exec":"music_voldwn.sh"},
+	{"pos":(0,550),"dia":(50,50),"img":"btn_exit.png","screen":"exit"})
 btnMap['exit'] = ({"pos":(100,100),"dia":(800,400),"col":(255,128,128,128)},
 	{"pos":(150,150),"dia":(325,300),"col":(0,255,0),"exec":"quit.sh"},
 	{"pos":(525,150),"dia":(325,300},"col":(255,0,0),"screen":"main"})
