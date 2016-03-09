182c11a99df21c9b1c63ae9dac6129daf7ea5a8f
electronicfreak <github@electronicfreak.de>
2016-03-09 23:46:24 +0100

    kommentare

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ screensaver = 5 * 30
 
 # ({pos:(x,y),dia:(w,h),col:(R,G,B),text:"text",img:"path",exec:""},...)
 btnMap = {}
+
+# hauptmenü
 btnMap['main'] = (
 	{"pos":(0,0),"dia":(70,100),"img":"menu_header.png"},
 	{"pos":(125,100),"dia":(200,75),"img":"btn_music_toggle.png","exec":"music_toggle.sh"},
@@ -19,16 +21,25 @@ btnMap['main'] = (
 	{"pos":(500,200),"dia":(400,20),"var":"music_artist","text":"no Artist","col":(255,255,255)},
 	{"pos":(500,300),"dia":(400,20),"var":"music_title","text":"no Titel","col":(255,255,255)}
 )
+
+# runterfahr option
 btnMap['exit'] = (
 	{"pos":(100,100),"dia":(800,400),"col":(255,128,128,128)},
 	{"pos":(150,150),"dia":(325,300),"col":(0,255,0),"exec":"quit.sh"},
 	{"pos":(525,150),"dia":(325,300),"col":(255,0,0),"screen":"main"}
 )
 
+# lichtsteuerung
 btnMap['light'] = (
 	{"pos":(0,0),"dia":(70,100),"img":"menu_header.png","screen":"main"}
 )
 
+# infrarot fernbedienung
+btnMap['ir'] = (
+	{"pos":(0,0),"dia":(70,100),"img":"menu_header.png","screen":"main"}
+)
+
+# system infos
 btnMap['sysinfo'] = (
 	{"pos":(0,0),"dia":(70,100),"img":"menu_header.png","screen":"main"},
 	{"pos":(125,100),"dia":(100,20),"text":"IP-Addr :","col":(255,255,255)},
