c1fdff2b3d1f27977692a19ffa1c4d973245fc32
homecontrol <github@electronicfreak>
2016-03-09 17:53:49 +0100

    test

---
+++ test_rss.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import socket,json
+
+def send(var,txt):
+        HOST = '127.0.0.1'
+        PORT = 5679
+        try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((HOST, PORT))
+                data = {'text':str(msg),'var':str(var)}
+                d = json.dumps(data)
+                s.sendall(d)
+                data = s.recv(1024)
+                s.close()
+                return data
+        except:
+                return False
+
+send('rss','BLA')
+
